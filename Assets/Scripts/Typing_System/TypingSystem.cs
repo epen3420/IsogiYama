b5512e2ed5c131c4e46bb2e7539a0666dd72a28d
epen3420 <epen3420@gmail.com>
2025-05-01 18:59:07 +0900

    feat: implement typing system using CSV file

--- Assets/Scripts/Typing_System/TypingSystem.cs
+++ Assets/Scripts/Typing_System/TypingSystem.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,12 +7,15 @@ using UnityEngine.InputSystem;
 public class TypingSystem : MonoBehaviour
 {
     private TypingJudger typingJudge;
+    private List<TypingQuest> questList;
     private int questIndex = 0;
 
     [SerializeField]
-    private List<string> questList = new List<string>();
+    private TextAsset csvFile;
     [SerializeField]
-    private TMP_Text questionText;
+    private TMP_Text japaneseText;
+    [SerializeField]
+    private TMP_Text romaText;
     [SerializeField]
     private TMP_Text inputText;
 
@@ -28,6 +32,7 @@ public class TypingSystem : MonoBehaviour
 
     private void Start()
     {
+        questList = LoadCSV(csvFile);
         Init();
     }
 
@@ -36,16 +41,18 @@ public class TypingSystem : MonoBehaviour
         if (questIndex >= questList.Count)
         {
             DisableKeyboardInput();
-            questionText.text = "Game Clear";
+            japaneseText.text = "Game Clear";
+            romaText.text = "Game Clear";
             inputText.text = null;
             return;
         }
 
-        string currentString = questList[questIndex];
-        questionText.text = currentString;
+        TypingQuest currentQuest = questList[questIndex];
+        japaneseText.text = currentQuest.japanese;
+        romaText.text = currentQuest.roma;
         inputText.text = null;
 
-        typingJudge = new TypingJudger(currentString);
+        typingJudge = new TypingJudger(currentQuest.roma);
         questIndex++;
     }
 
@@ -70,6 +77,34 @@ public class TypingSystem : MonoBehaviour
         }
     }
 
+    private List<TypingQuest> LoadCSV(TextAsset csvFile)
+    {
+        StringReader reader = new StringReader(csvFile.text);
+        var questions = new List<TypingQuest>();
+
+        bool isHeader = true;
+
+        while (reader.Peek() > -1)
+        {
+            var line = reader.ReadLine();
+            if (isHeader)
+            {
+                isHeader = false; // 最初の1行はヘッダーとしてスキップ
+                continue;
+            }
+
+            var values = line.Split(',');
+
+            if (values.Length >= 2)
+            {
+                questions.Add(new TypingQuest(values[0], values[1]));
+            }
+        }
+
+        reader.Close();
+        return questions;
+    }
+
     private void EnableKeyboardInput()
     {
         var keyboard = Keyboard.current;
@@ -86,5 +121,17 @@ public class TypingSystem : MonoBehaviour
 
         // キーボード入力の受取り解除
         keyboard.onTextInput -= OnKeyboardInput;
+    }
+}
+
+class TypingQuest
+{
+    public string japanese { get; private set; }
+    public string roma { get; private set; }
+
+    public TypingQuest(string japanese, string roma)
+    {
+        this.japanese = japanese;
+        this.roma = roma;
     }
 }
