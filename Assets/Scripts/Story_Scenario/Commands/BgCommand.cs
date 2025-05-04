bb0d7a6100c53f9f00f199dbf5fcd454aac7cfee
コトレ <cottonstar.watahoshi@gmail.com>
2025-05-05 00:09:10 +0900

    Feat BgCommand be able to forget
    Forgetできるようにした

--- Assets/Scripts/Story_Scenario/Commands/BgCommand.cs
+++ Assets/Scripts/Story_Scenario/Commands/BgCommand.cs
@@ -14,6 +14,13 @@ namespace IsogiYama.Commands
         public override async UniTask ExecuteAsync(LineData<ScenarioFields> lineData)
         {
             string bgNames = lineData.Get<string>(ScenarioFields.Arg1);
+            string isForget = lineData.Get<string>(ScenarioFields.Arg2);
+
+            if(isForget == "Forget")
+            {
+                vfxController.ChangeBackgroundAsync(bgNames).Forget();
+                return;
+            }
 
             await vfxController.ChangeBackgroundAsync(bgNames);
         }
