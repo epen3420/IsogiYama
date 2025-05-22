8e0ee9ae249b8969c1c68310402fd146259d735b
コトレ <cottonstar.watahoshi@gmail.com>
2025-05-22 23:57:50 +0900

    Add: TypingResult and ResultHolder
    タイピングのリザルト表示に必要なデータを受け渡すメソッドと
    シーンをまたいでそのデータを保持しておくホルダークラスの実装

---
+++ Assets/Scripts/Result/ResultHolder.cs
@@ -0,0 +1,10 @@
+﻿public class TypingResultManager : Singleton<TypingResultManager>
+{
+    private TypingResult result = new TypingResult();
+
+    public TypingResult GetResult() => result;
+
+    public void SetResult(TypingResult r) => result = r;
+
+    public void ClearResult() => result = new TypingResult();
+}
